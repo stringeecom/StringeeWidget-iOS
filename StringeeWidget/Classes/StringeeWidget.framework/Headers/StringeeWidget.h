23bacb65aada3f11be3dbd6dbb85c277cae337f2
stringeecom <info@stringee.com>
2022-06-23 10:50:02 +0700

    Add StringeeWidget.framework file

---
+++ StringeeWidget/Classes/StringeeWidget.framework/Headers/StringeeWidget.h
@@ -0,0 +1,18 @@
+//
+//  StringeeWidget.h
+//  StringeeWidget
+//
+//  Created by HoangDuoc on 6/7/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StringeeWidget.
+FOUNDATION_EXPORT double StringeeWidgetVersionNumber;
+
+//! Project version string for StringeeWidget.
+FOUNDATION_EXPORT const unsigned char StringeeWidgetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringeeWidget/PublicHeader.h>
+
+
